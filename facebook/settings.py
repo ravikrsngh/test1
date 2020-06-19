161f75aa4bb8eb51e73213240d10c5b71a74b3cb
Ravi Kumar Singh <ravikrsngh.rks@gmail.com>
2020-06-19 20:52:20 +0530

    abc

--- facebook/settings.py
+++ facebook/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -122,3 +123,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
